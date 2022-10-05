70f4ad0ef274f4796ddb9a42523d9f8e2a448bdf
kook <jumice10@gmail.com>
2022-10-05 18:32:48 +0900

    Initial Commit

---
+++ SUIsample/SUIsampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUIsampleApp.swift
+//  SUIsample
+//
+//  Created by kook on 2022/10/05.
+//
+
+import SwiftUI
+
+@main
+struct SUIsampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
